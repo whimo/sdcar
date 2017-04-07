35a50f2f344a0a086a352a62d33766fc3f903d97
Artemy Belousov <whitemoustache01@gmail.com>
2017-04-07 22:04:02 +0300

    Added mainloop and ROI edges

--- main.py
+++ main.py
@@ -3,18 +3,33 @@ import numpy as np
 
 from calibrate import calibrate_camera
 import processing
+from stream import Stream
+
+
+ROI_HEIGHT = 0.45
+ROI_TOP = 0.15
+ROI_BOTTOM = 0.8
+
 
 
 def main():
-    retval, camera_matrix, dist_coeffs, rvecs, tvecs = calibrate_camera()
+    retval, camera_matrix, dict_coeffs, rvecs, tvecs = calibrate_camera()
+    stream = Stream(url, camera_matrix, dict_coeffs)
 
-    image = cv2.imread('road1.jpg')
-    #image = processing.undistort(image, camera_matrix, dist_coeffs)
-    image = processing.edges(image)
-    lines = processing.lines(image)
+    while True:
+        stream.update()
+        image = stream.frame
+        shape = image.shape
 
-    cv2.imshow('edges', image)
-    cv2.waitKey(0)
+        roi_edges = np.array([[
+                            ((shape[1] * (1 - ROI_BOTTOM)) // 2, shape[0]),
+                            ((shape[1] * (1 - ROI_TOP)) // 2, shape[0] - shape[0] * ROI_HEIGHT),
+                            (shape[1] - (shape[1] * (1 - ROI_TOP)) // 2, shape[0] - shape[0] * ROI_HEIGHT),
+                            (shape[1] - (shape[1] * (1 - ROI_BOTTOM)) // 2, shape[0])]],
+                            dtype = np.int32)
+
+        cv2.waitKey(1)
+
 
 
 if __name__ == '__main__':
